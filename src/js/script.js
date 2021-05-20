489907d8c74b41c1895e7b69385750b9c0edf68d
Maxsim Sadovnikov <majestyzz@yandex.ru>
2021-05-20 22:54:47 +0400

    jsLess2

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,11 @@
 "use strict";
 
-const numberOfFilms = prompt('How many films do you watch?');
+let numberOfFilms;
+
+do {
+  numberOfFilms = prompt('Как много фильмов вы смотрите?', '');
+} while(numberOfFilms == "" || numberOfFilms === null || numberOfFilms.length > 50)
+
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -9,11 +14,27 @@ const personalMovieDB = {
   privat: false
 };
 
+if (personalMovieDB['count'] < 10) {
+  alert("Просмотрено довольно мало фильмов")
+} else if (personalMovieDB['count'] >= 10 && (
+  personalMovieDB['count']) <= 30) {
+        alert("Вы обычный зритель");
+      } else if (personalMovieDB['count'] > 30) {alert('Вы киноман!')}
+      else alert("Ошибка!");
+
+
 for (let i = 0; i < 2; i++) {
-  let a = prompt('Один из последних просмотренных фильмов?');
-  let b = prompt('На сколько оцените его?');
-  personalMovieDB['movies'][a] = b;
-}
+  const a = prompt('Один из последних просмотренных фильмов?'),
+        b = prompt('На сколько оцените его?');
+  if (a != null && b != null && (a != '' && 
+    b != '') && a.length < 50) {
+      console.log('done');
+      personalMovieDB['movies'][a] = b;
+    } else {
+      console.log('error');
+      i--;
+    }
+} 
 
 console.log(personalMovieDB);
 
